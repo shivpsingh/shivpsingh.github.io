19ee1cdde369a4119178fc1724bc716bdf4acb1d
Shiv Pratap Singh <shiv.safari@gmail.com>
2021-08-06 16:50:31 +0530

    Added Nav Function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS Block
+
+let navTrigger = document.getElementById('nav-trigger');
+let navitems = document.getElementById('navitems');
+
+navTrigger.addEventListener('click', function() {
+    if(navitems.style.display == "" || navitems.style.display == "none") {
+        navitems.style.display = "block";
+    } else if (navitems.style.display == "block") {
+        navitems.style.display = "none";
+    }
+});
