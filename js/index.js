c022355b455204718cb22ae070a28b4b5d1b8112
Shiv Pratap Singh <shiv.safari@gmail.com>
2021-08-22 22:35:17 +0530

    Fix Issue of Mobile Navigation
    While testing the responsiveness, found one bug:
    
    If the mobile nav is once opened and then closed, streching the browser window is not making any changes to navitems, due to which navbar was not visible.
    
    Fixed this issue by adding additional classes and removing it using classList method in javascript.

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ let navTrigger = document.getElementById('nav-trigger');
 let navitems = document.getElementById('navitems');
 
 navTrigger.addEventListener('click', function() {
-    if(navitems.style.display == "" || navitems.style.display == "none") {
-        navitems.style.display = "block";
-    } else if (navitems.style.display == "block") {
-        navitems.style.display = "none";
+    if(navitems.classList.contains('disp-none')) {
+        navitems.classList.remove('disp-none');
+        navitems.classList.add('disp-block');
+    } else if (navitems.classList.contains('disp-block')) {
+        navitems.classList.remove('disp-block');
+        navitems.classList.add('disp-none');
     }
 });
